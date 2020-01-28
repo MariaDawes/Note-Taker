5ec3e2d0ae4de7c582ab16be1131a8906ef1a15b
MariaDawes <maria.c.dawes@gmail.com>
2020-01-27 20:35:24 -0600

    code started not to work

--- server.js
+++ server.js
@@ -1,57 +1,66 @@
-const express = require("express");
-const fs = require("fs");
-const path = require("path");
-const datab = require('./db/db.json');
-const uuid = require("uuid/v1");
-let dbpath = path.join(__dirname, '/db/db.json');
+const express = require('express');
+const fs = require('fs');
 
-//Express app and PORT
+//Variables for path
+const path = require('path');
+const db = require('./db/db.json');
+let data_path = path.join(__dirname, '/db/db.json');
+
+//Variable for uuid
+const uuid = require("uuid/v1");
+
+//Variables for Express app and PORT
 const app = express();
 const PORT = process.env.PORT || 3030;
 
-//App to handle data parsing
+//linking public folder to get data from it
+app.use(express.static('public'));
+
+//setting up application parsing for my JSON files
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//Use files in directory name public
-app.use(express.static('public'));
-
-//Page 1 - index.html. Send file when called
+//Page 1 - set index.html file
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-//Page 2 - notes.html - Send file when called 
+//Page 2 - set notes.html
 app.get('/notes', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/notes.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-//Get new note and save it on JSON
+//Getting my API/Notes
 app.get('/api/notes', function(req, res) {
-  res.json(datab);
+    res.json(db);
 });
+
+//adding random id to the notes and saving it.
 app.post("/api/notes", function(req, res) {
-  var uniId = uuid();
-  var newtask = req.body;
-  newtask.id = uniId;
-  datab.push(newtask);
-  fs.writeFileSync(dbpath,JSON.stringify(datab),function(err,data){
-      if (err) throw err;
-      })
-  res.json(newtask);
+  
+  var uniqueId = uuid();
+  var newnote = req.body;
+  newnote.id = uniqueId;
+
+  db.push(newnote);
+  fs.writeFileSync(data_path,JSON.stringify(db),function(err,data){
+    if (err) throw err;
+  })
+  res.json(newnote);
 });
 
+//Delete notes 
+app.delete("/api/notes/:id" ,function(req,res){
 
-//Delete note
-app.delete("/api/notes/:id", function(req,res){
-  const delId = req.params.id;
-  for (i=0; i<datab.length; i++){
-    if(datab[i].id ===delId){
-        datab.splice(i,1);
-        break;
+  const deleteId = req.params.id;
+  for (i=0;i<db.length;i++){
+
+    if(db[i].id ===deleteId){
+      db.splice(i,1);
+      break;
     } 
-  }      
-  res.json(datab);
+  }
+  res.json(db);
 });
 
 app.listen(PORT, function () {
