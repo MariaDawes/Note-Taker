e99afbd856fedc92e38c5b3220b62ca14c5727c1
MariaDawes <maria.c.dawes@gmail.com>
2020-01-27 19:09:58 -0600

    code working

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const db = require('./db/db.json');
-const uuid = require("uuid/v1");//this is needed to generate random id for the notes.
-//we need to install npm i uuid
+const datab = require('./db/db.json');
+const uuid = require("uuid/v1");
+let dbpath = path.join(__dirname, '/db/db.json');
 
-let data_path = path.join(__dirname, '/db/db.json');
-
-//Set up the "Express" app and PORT
+//Express app and PORT
 const app = express();
 const PORT = process.env.PORT || 3030;
 
-//app to handle data parsing
+//App to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//Code to data files in directory name public
+//Use files in directory name public
 app.use(express.static('public'));
 
 //Page 1 - index.html. Send file when called
@@ -28,37 +26,34 @@ app.get('/notes', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-//Get new note
+//Get new note and save it on JSON
 app.get('/api/notes', function(req, res) {
-  res.json(db);
+  res.json(datab);
 });
-
-//Save new note on JSON
 app.post("/api/notes", function(req, res) {
-  
-  var uniqueId = uuid();
-  var newnote = req.body ;
-  newnote.id = uniqueId;
-  db.push(newnote);
-  fs.writeFileSync(data_path,JSON.stringify(db),function(err,data){
+  var uniId = uuid();
+  var newtask = req.body;
+  newtask.id = uniId;
+  datab.push(newtask);
+  fs.writeFileSync(dbpath,JSON.stringify(datab),function(err,data){
       if (err) throw err;
       })
-  res.json(newnote);
+  res.json(newtask);
 });
 
 
-//Delete notes by id.Here we are passing id to the url.
-app.delete("/api/notes/:id" ,function(req,res){
-
-  //we are getting id information from the url.
-  const deleteId = req.params.id;
-  for (i=0;i<db.length;i++){
-    if(db[i].id ===deleteId){
-      //
-          db.splice(i,1);
-          break;
-        } 
+//Delete note
+app.delete("/api/notes/:id", function(req,res){
+  const delId = req.params.id;
+  for (i=0; i<datab.length; i++){
+    if(datab[i].id ===delId){
+        datab.splice(i,1);
+        break;
+    } 
   }      
-  res.json(db);
+  res.json(datab);
 });
 
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
